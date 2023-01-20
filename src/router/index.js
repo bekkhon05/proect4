932a392530c192e233cb6dd6d1636c35d406c6bf
bekkhon05 <qambarovbekkhon@gmail.com>
2023-01-20 11:12:53 +0500

    start

---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+// import React from "react";
+// import { Routes, Route} from "react-router-dom";
+// import Home from "./pages/home/index"
+
+// const Router = () => {
+//   return (
+//     <Routes>
+//       <Route path="/" element={<Home />} />
+//       
+//     </Routes>
+//   );
+// };
+
+// export default Router;
